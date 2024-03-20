fcd32b0353f086b2791f8b2d9ef4ec2cdcbf917e
Averyden <you@example.com>
2024-03-20 10:27:15 +0100

    build UI class

--- src/passMain.py
+++ src/passMain.py
@@ -1,6 +1,19 @@
-
+import tkinter as tk
+import tkinter.ttk as ttk
 from passData import PassData
+from uiBuild import BuildFunctions
 
+builder = BuildFunctions()
 data = PassData()
 
-data.createTables() 
+class PassManger(ttk.Frame):
+    def __init__(self,root):
+        self.root = root
+        self.root.title("TestApp for passDB")
+
+        self.buildUIStart1()
+
+
+    def buildUIStart1(self): #* Prompt user with login / register user button
+        pass
+
