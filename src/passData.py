eea6e3bc00e20a467158f54cc9d7e3645740e737
Averyden <you@example.com>
2024-03-20 08:54:05 +0100

    setup main function layout

--- src/passData.py
+++ src/passData.py
@@ -5,6 +5,25 @@ class PassData():
         self.db = sqlite3.connect("assets/passData.db")
 
         
+    def createUser(self):
+        pass
+
+    def createPassword(self):
+        pass
+
+    def deleteUser(self):
+        pass #! MAKE SURE IT ALSO DELETES ALL THE PASSWORDS ASSOCIATED TO THE SAME USER
+
+    def deletePassword(self):
+        pass
+
+    def editPassword(self):
+        pass
+
+    def updateUser(self): #* Updating user details incase they get a new email.
+        pass
+
+    
     def createTables(self):
         c = self.db.cursor()
         try: 
@@ -22,14 +41,13 @@ class PassData():
             c.execute('''
             CREATE TABLE Passwords (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                owner INT, 
-                SERVICE TEXT,
+                owner INTEGER, 
+                service TEXT,
                 password TEXT,
-                locked INT);
+                locked INTEGER);''')
                 #* Supposed to be a boolean value, but SQLite doesn't support bools natively.
                 #* So good ol' INT coming to save the day here.
-                #* 0 for false, 1 for true.
-            ''')
+                #* 0 for false, 1 for true.   
         except:
             print("Table 'Passwords' already exists... \n Passing...")
             
