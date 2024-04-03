1729b0ff73721d80bce8a4e8d6821df71e03569a
Averyden <you@example.com>
2024-04-03 10:44:39 +0200

    Deprecated building class, making use of the forget property

--- src/passMain.py
+++ src/passMain.py
@@ -2,10 +2,10 @@ import tkinter as tk
 import tkinter.ttk as ttk
 import json
 from passData import PassData
-from uiBuild import BuildFunctions
+# from uiBuild import BuildFunctions
 
 
-builder = BuildFunctions()
+# builder = BuildFunctions()
 data = PassData()
 
 data.createTables()
@@ -23,8 +23,17 @@ except Exception as e:
 class PassManger(tk.Frame):  
     def __init__(self):
         tk.Frame.__init__(self)
-        self.buildUIStart1()
         self.updateLabels()
+        self.currentUID = None
+
+        self.updateLoginWindow()
+
+
+    def updateLoginWindow(self):
+        if lastLoggedUser["LastLogDict"]["Loggedin"]:
+            self.buildMainWindow()
+        else:
+            self.buildUIStart1() #! Build login window based on user login status.
 
     def updateLabels(self):
         try:
@@ -35,66 +44,90 @@ class PassManger(tk.Frame):
         if lastLoggedUser["LastLogDict"]["Loggedin"] == True:
             self.lblSlct.configure(text=f"Welcome, {data.getUserFromID(lastLoggedUser['LastLogDict']['Loggedin'])}.")
 
+    def clearWidgets(self):
+        for widget in self.winfo_children():
+            widget.grid_forget() #? Should clear all widgets?
+
     def logOut(self):
         lastLoggedUser["LastLogDict"]["Loggedin"] = False
         lastLoggedUser["LastLogDict"]["lastLoggedUser"] = "none"
         
         with open("assets/lastLogin.json", "w") as lastLogin:
             json.dump(lastLoggedUser, lastLogin, indent=4)
+        
+        self.updateLoginWindow()
 
     def sendLoginDetails(self):
         usernameToSend = self.userNameEntry.get()
         passwordToSend = self.passwordEntry.get()
         data.loginUser(userName=usernameToSend, password=passwordToSend)
-        self.updateLabels()
+        # self.updateLabels()
+        self.updateLoginWindow()
 
-    def callBuildRegister(self):
-        builder.buildRegisterUI()
+    # def callBuildRegister(self):
+    #     builder.buildRegisterUI()
 
+
+    #! CONTINUING FORTH IS THE BUILDING OF ALL THE UIS
+    #! This code is about to get messy really quickly
+    #! Such is the code of ui building
+    #! 😔
 
     def buildUIStart1(self): 
+        self.clearWidgets()
         self.Frame = tk.Frame(self)
         self.Frame.grid(column=0, row=0)
 
-        if lastLoggedUser["LastLogDict"]["Loggedin"] == False: #* Check if a user was seen logged in, if not, prompt with buttons
-            print("No user found to be logged in since last time running")
-            self.lblSlct = ttk.Label(self.Frame, text="Please login or create account.")
-            self.lblSlct.grid(column=0, row=0, columnspan=2)
+    
+        print("No user found to be logged in since last time running")
+        self.lblSlct = ttk.Label(self.Frame, text="Please login or create account.")
+        self.lblSlct.grid(column=0, row=0, columnspan=2)
 
-            self.lblUN = ttk.Label(self.Frame, text="Username")
-            self.lblUN.grid(row=1, column=0, columnspan=2)
+        self.lblUN = ttk.Label(self.Frame, text="Username")
+        self.lblUN.grid(row=1, column=0, columnspan=2)
 
-            self.userNameEntry = ttk.Entry(self.Frame, text="Username")
-            self.userNameEntry.grid(row=2, column=0, columnspan=2)
+        self.userNameEntry = ttk.Entry(self.Frame, text="Username")
+        self.userNameEntry.grid(row=2, column=0, columnspan=2)
 
-            self.lblPass = ttk.Label(self.Frame, text="Password")
-            self.lblPass.grid(row=3, column=0, columnspan=2)
+        self.lblPass = ttk.Label(self.Frame, text="Password")
+        self.lblPass.grid(row=3, column=0, columnspan=2)
 
-            self.passwordEntry = ttk.Entry(self.Frame, text="Password", show="\u2022") #! u2022 is for the bullet symbol to hide the user's password.and
-            #? Maybe incorporate a "show" button?
-            #? Depends on if TKinter allows that.
-            self.passwordEntry.grid(row=4, column=0, columnspan=2)
+        self.passwordEntry = ttk.Entry(self.Frame, text="Password", show="\u2022") #! u2022 is for the bullet symbol to hide the user's password.and
+        #? Maybe incorporate a "show" button?
+        #? Depends on if TKinter allows that.
+        self.passwordEntry.grid(row=4, column=0, columnspan=2)
 
-            self.lblAccountIssue = ttk.Label(self.Frame, text="")
-            self.lblAccountIssue.grid(row=5, column=0, columnspan=2)
+        self.lblAccountIssue = ttk.Label(self.Frame, text="")
+        self.lblAccountIssue.grid(row=5, column=0, columnspan=2)
 
-            self.btnLogin = ttk.Button(self.Frame, text="Log in", command=self.sendLoginDetails) 
-            self.btnLogin.grid(row=6, column=0)
+        self.btnLogin = ttk.Button(self.Frame, text="Log in", command=self.sendLoginDetails) 
+        self.btnLogin.grid(row=6, column=0)
 
-            self.btnRegister = ttk.Button(self.Frame, text="Create account", command=None) #! Pass for now
-            self.btnRegister.grid(row=6, column=1)
+        self.btnRegister = ttk.Button(self.Frame, text="Create account", command=None) #! Pass for now
+        self.btnRegister.grid(row=6, column=1)
 
-        else:
-            print("Wow!")
-            self.lblSlct = ttk.Label(self.Frame, text="Welcome.. \nif you see this, the update label function failed...")
-            self.lblSlct.grid(column=0, row=0, columnspan=2)
-
-            self.btnToggle = ttk.Button(self.Frame, text="Log out", command=self.logOut)
-            self.btnToggle.grid(column=0, row=1, columnspan=2)
-        
+       
+        print("Wow!")
+      
         
 
         self.pack()
+
+
+    def buildMainWindow(self):
+        self.clearWidgets()
+        self.Frame = tk.Frame(self)
+        self.Frame.grid(column=0, row=0)
+
+
+        self.lblSlct = ttk.Label(self.Frame, text="Welcome.. \nif you see this, the update label function failed...")
+        self.lblSlct.grid(column=0, row=0, columnspan=2)
+
+        self.btnToggle = ttk.Button(self.Frame, text="Log out", command=self.logOut)
+        self.btnToggle.grid(column=0, row=1, columnspan=2)
+
+        self.pack()
+    
 
 prg = PassManger()
 prg.master.title('PasManJSONTest')
