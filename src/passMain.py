12b2cb9d08efcedade9ba1e04c974cd5532881bb
Averyden <you@example.com>
2024-04-04 10:08:12 +0200

    Register UI

--- src/passMain.py
+++ src/passMain.py
@@ -32,6 +32,7 @@ class PassManger(tk.Frame):
 
         #* Reopen the file to read the updated content 
         #! WHY ISNT THERE A BETTER WAY TO DO THIS 😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭
+        #? There might be, not bothered to look for it tho :)
         with open("assets/lastLogin.json") as lastLogin:
             lastLoggedUser = json.load(lastLogin)
             
@@ -87,6 +88,24 @@ class PassManger(tk.Frame):
 
         print(f"Sender says: {lastLoggedUser['LastLogDict']['Loggedin']}")
 
+
+    def sendCreationDetails(self):
+        usernameToSend = self.userNameEntry.get()
+        passwordToSend = self.passwordEntry.get()
+        data.loginUser(userName=usernameToSend, password=passwordToSend)
+
+        #* Reopen the file to read the updated content 
+        #! WHY ISNT THERE A BETTER WAY TO DO THIS 😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭
+        with open("assets/lastLogin.json") as lastLogin:
+            lastLoggedUser = json.load(lastLogin)
+    
+        if len(data.loginIssue) != 0: #* Failed login, update labels to maintain user info on entries and display error msg.
+            self.updateLabels()
+        else: #* Successful login, update the entire window.
+            self.updateLoginWindow()
+
+        print(f"Sender says: {lastLoggedUser['LastLogDict']['Loggedin']}")
+
     #! CONTINUING FORTH IS THE BUILDING OF ALL THE UIS
     #! This code is about to get messy really quickly
     #! Such is the code of ui building
@@ -122,10 +141,50 @@ class PassManger(tk.Frame):
         self.btnLogin = ttk.Button(self.Frame, text="Log in", command=self.sendLoginDetails) 
         self.btnLogin.grid(row=6, column=0)
 
-        self.btnRegister = ttk.Button(self.Frame, text="Create account", command=None) #! Pass for now
+        self.btnRegister = ttk.Button(self.Frame, text="Create account", command=self.buildRegisterWindow) #! Pass for now
         self.btnRegister.grid(row=6, column=1)
         self.pack()
 
+
+    def buildRegisterWindow(self):
+        self.alzheimers()
+        self.Frame = tk.Frame(self)
+        self.Frame.grid(column=0, row=0)
+
+        self.lblSlct = ttk.Label(self.Frame, text="Create account.")
+        self.lblSlct.grid(column=0, row=0, columnspan=2)
+
+        self.lblUN = ttk.Label(self.Frame, text="Username")
+        self.lblUN.grid(row=1, column=0, columnspan=2)
+
+        self.userNameEntry = ttk.Entry(self.Frame, text="Username")
+        self.userNameEntry.grid(row=2, column=0, columnspan=2)
+
+        self.lblPass = ttk.Label(self.Frame, text="Password")
+        self.lblPass.grid(row=3, column=0, columnspan=2)
+
+        self.passwordEntry = ttk.Entry(self.Frame, text="Password", show="\u2022") #! u2022 is for the bullet symbol to hide the user's password.and
+        #? Maybe incorporate a "show" button?
+        #? Depends on if TKinter allows that.
+        self.passwordEntry.grid(row=4, column=0, columnspan=2)
+
+        self.lblRptPass = ttk.Label(self.Frame, text="Confirm password")
+        self.lblRptPass.grid(row=5, column=0, columnspan=2)
+
+        self.rptPassEntry = ttk.Entry(self.Frame, text="Confirm Password", show="\u2022")
+        self.rptPassEntry.grid(row=6, column=0, columnspan=2)
+
+
+        self.lblAccountIssue = ttk.Label(self.Frame, text="")
+        self.lblAccountIssue.grid(row=7, column=0, columnspan=2)
+
+        self.btnLogin = ttk.Button(self.Frame, text="Create account", command=self.sendCreationDetails) 
+        self.btnLogin.grid(row=8, column=0)
+
+        self.btnRegister = ttk.Button(self.Frame, text="Log in to existing account", command=self.buildUIStart1) #! Pass for now
+        self.btnRegister.grid(row=8, column=1)
+
+        self.pack()
 
     def buildMainWindow(self):
         print("AAAAA")
